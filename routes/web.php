1d4725b06006dff7a2680566e9cf4d9675295540
Kouki Nakayama <k.number.02@gmail.com>
2023-06-21 20:50:41 +0900

    laravel breeze導入

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IdeaController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,4 +14,21 @@ use App\Http\Controllers\IdeaController;
 |
 */
 
-Route::resource('ideas', IdeaController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::middleware('auth')->group(function () {
+    Route::resource('ideas', IdeaController::class);
+});
+
+require __DIR__ . '/auth.php';
